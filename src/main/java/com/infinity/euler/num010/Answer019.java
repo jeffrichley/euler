e132af8412184e6b729479d0ff993e179e7f6752
jeffrichley <jeffrichley@gmail.com>
2017-06-01 10:10:01 -0400

    completed answer 19

---
+++ src/main/java/com/infinity/euler/num010/Answer019.java
@@ -0,0 +1,28 @@
+package com.infinity.euler.num010;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Answer19 {
+
+	public static void main(String[] args) {
+		
+		int numSundays = 0;
+		
+		for (int year = 1901; year <= 2000; year++) {
+			for (int month = 0; month <= 11; month++) {
+				Calendar date = Calendar.getInstance();
+				date.set(year, month, 1);
+
+				int dayOfWeek = date.get(Calendar.DAY_OF_WEEK);
+				if (dayOfWeek == 1) {
+					numSundays++;
+				}
+			}
+		}
+		
+		System.out.println("There were " + numSundays + " sundays");
+		
+	}
+
+}

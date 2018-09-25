f210bafe1cd6687adcb9596ac4092de57413b7e7
Jeff Richley <jeffrichley@gmail.com>
2018-09-25 07:32:15 -0400

    adding more base cases

--- src/main/java/com/infinity/euler/num620/Answer629.java
+++ src/main/java/com/infinity/euler/num620/Answer629.java
@@ -1,39 +1,43 @@
 package com.infinity.euler.num620;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Answer629 {
 	
-	private static int n = 4;
-	private static int k = 3;
+	private static int n = 6;
+	private static int k = 6;
 	
 
+//	public static void main(String[] args) {
+//		
+//		// winning position f(5,2)=3
+////		System.out.println(isWinning(1,1,1,2));
+////		System.out.println(isWinning(1,1,3));
+////		System.out.println(isWinning(1,4));
+////		System.out.println(isWinning(2,3));
+//		
+//		// winning position f(5,3)=5
+////		System.out.println(isWinning(1,1,1,2));
+////		System.out.println(isWinning(1,1,3));
+////		System.out.println(isWinning(1,4));
+////		System.out.println(isWinning(2,3));
+////		System.out.println(isWinning(5));
+//		
+//		// get all the combinations for starting piles
+//		new Answer629().run();
+//		
+//	}
+	
 	public static void main(String[] args) {
-		
-		// winning position f(5,2)=3
-//		System.out.println(isWinning(1,1,1,2));
-//		System.out.println(isWinning(1,1,3));
-//		System.out.println(isWinning(1,4));
-//		System.out.println(isWinning(2,3));
-		
-		// winning position f(5,3)=5
-//		System.out.println(isWinning(1,1,1,2));
-//		System.out.println(isWinning(1,1,3));
-//		System.out.println(isWinning(1,4));
-//		System.out.println(isWinning(2,3));
-//		System.out.println(isWinning(5));
-		
-		// get all the combinations for starting piles
 		new Answer629().run();
-		
 	}
+	
 
 	private void run() {
-		int n = 6;
-		int k = 6;
 		
 		List<int[]> staring = getStartingCombos(n, k);
 	}
@@ -50,7 +54,7 @@ public class Answer629 {
 			}
 		}
 		
-		// set up the base cases
+		// set up the base cases in number of rocks 2
 		for (int tmpRocks = 2; tmpRocks <= numRocks; tmpRocks++) {
 			int i = 1;
 			int j = tmpRocks - i;
@@ -62,7 +66,35 @@ public class Answer629 {
 			}
 		}
 		
+		// set up the base cases for the diagonal
+		for (int i = 3; i <= numRocks && i <= maxPiles; i++) {
+			int[] option = new int[i];
+			Arrays.fill(option, 1);
+			table[i][i].addOption(option);
+		}
 		
+		
+		
+		for (int rocks = 3; rocks <= numRocks; rocks++) {
+			for (int piles = 3; piles < rocks; piles++) {
+				int maxPilePieces = rocks - piles + 1;
+				System.out.println(rocks + " " + piles + " " + maxPilePieces);
+				
+				List<int[]> possible = new ArrayList<>();
+				int pileLookup = piles - 1;
+				for (int tmp = maxPilePieces; tmp > 1; tmp--) {
+					int rockLookup = rocks - tmp;
+					Cell lookup = table[rockLookup][pileLookup];
+					System.out.print("\t");
+					for (int[] lookupOption : lookup.getOptions()) {
+						for (int t : lookupOption) {
+							System.out.print(t+",");
+						}
+						System.out.println(tmp);
+					}
+				}
+			}
+		}
 		
 //		for (int rocks = 3; rocks <= numRocks; rocks++) {
 //			for (int piles = 2; piles <= maxPiles; piles++) {

f191b2f43c170dee0b5b7896ff8f224cc8d9dc3d
jeffrichley <jeffrichley@gmail.com>
2018-10-11 14:51:04 -0400

    precalculating all grundy numbers for k = 2 -> n

--- src/main/java/com/infinity/euler/num620/Answer629.java
+++ src/main/java/com/infinity/euler/num620/Answer629.java
@@ -1,5 +1,8 @@
 package com.infinity.euler.num620;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Jeffrey.Richley
  * 
@@ -11,7 +14,7 @@ package com.infinity.euler.num620;
  */
 public class Answer629 {
 
-	public static final int NUM_ROCKS = 8;
+	public static final int NUM_ROCKS = 200;
 	public static final int MAX_PILES = NUM_ROCKS;
 	
 	public static void main(String[] args) {
@@ -23,6 +26,8 @@ public class Answer629 {
 		// all of the options and calculate if it would be winning or not
 		long numWinners = calculateWinners(nimbers);
 		
+		System.out.println();
+		
 //		printArray(nimbers);
 	}
 
@@ -31,28 +36,65 @@ public class Answer629 {
 		int[][] nimbers = new int[NUM_ROCKS+1][MAX_PILES+1];
 		
 		// calculate k = 2
-		for (int i = 1; i < NUM_ROCKS; i++) {
+		for (int i = 1; i <= NUM_ROCKS; i++) {
 			nimbers[i][2] = (i+1) % 2;
 		}
 
 		// we now need where k = 3
 		for (int n = 2; n <= MAX_PILES; n++) {
-			System.out.println("Processing " + n);
+//			System.out.println("Processing " + n);
+			
+			Set<Integer> grundys = new HashSet<>();
+			// look at k = 2
 			for (int first = 1; first <= n / 2; first++) {
 				int second = n - first;
 				int grundy = nimbers[first][3] ^ nimbers[second][3];
-				System.out.println(first + " " + second + " = " + grundy);
+				grundys.add(grundy);
+//				System.out.println(first + " " + second + " : " + grundy);
 			}
+			
+			// now add the k = 3
+			if (n >= 3) {
+				for (int first = 1; first <= n / 3; first++) {
+					for (int second = first; second <= n - (first + second); second++) {
+						int third = n - (first + second);
+						
+						int g1 = nimbers[first][3];
+						int g2 = nimbers[second][3];
+						int g3 = nimbers[third][3];
+						
+						int grundy = g1 ^ g2 ^ g3;
+						grundys.add(grundy);
+						
+//						System.out.println(first + " " + second + " " + third + " : " + grundy);
+					}
+				}
+			}
+			
+			int mex = findMex(grundys);
+			nimbers[n][3] = mex;
+			
+//			System.out.println("Grundy for " + n + " is " + mex);
 		}
 		
 		// calculate k >= 4
 		for (int j = 4; j <= MAX_PILES; j++) {
-			for (int i = 1; i < NUM_ROCKS; i++) {
+			for (int i = 1; i <= NUM_ROCKS; i++) {
 				nimbers[i][j] = i - 1;
 			}
 		}
 		
 		return nimbers;
+	}
+
+	private static int findMex(Set<Integer> grundys) {
+		int mex = 0;
+		
+		while (grundys.contains(mex)) {
+			mex++;
+		}
+		
+		return mex;
 	}
 
 	private static long calculateWinners(int[][] nimbers) {
@@ -61,7 +103,7 @@ public class Answer629 {
 	}
 
 	private static void printArray(int[][] array) {
-		int length = array.length-1;
+		int length = array.length;
 		for (int i = 0; i < length; i++) {
 			printArray(array[i]);
 			System.out.println();
